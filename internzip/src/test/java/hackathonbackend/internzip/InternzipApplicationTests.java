195bfeba4e985dc2e8f636cd63b371a077673beb
Mark Moawad <msmoawad99@gmail.com>
2021-02-26 18:18:43 -0500

    react and spring app

---
+++ internzip/src/test/java/hackathonbackend/internzip/InternzipApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathonbackend.internzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
