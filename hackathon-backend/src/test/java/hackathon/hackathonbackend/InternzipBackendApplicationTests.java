bf98c357950eab15d908c32036fe1922ec534fce
Mark Moawad <msmoawad99@gmail.com>
2021-02-26 18:30:43 -0500

    spring init

--- hackathon-backend/src/test/java/hackathon/hackathonbackend/InternzipBackendApplicationTests.java
+++ hackathon-backend/src/test/java/hackathon/hackathonbackend/InternzipBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package hackathonbackend.internzip;
+package hackathon.hackathonbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InternzipApplicationTests {
+class InternzipBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
