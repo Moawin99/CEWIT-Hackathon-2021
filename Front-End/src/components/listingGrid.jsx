031bc168c7315a6aa6322ea5517de52023e144ec
alex <alexperez52@gmail.com>
2021-02-27 18:01:03 -0500

    Listings page and pagination done

--- Front-End/src/components/listingGrid.jsx
+++ Front-End/src/components/listingGrid.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
     Grid
 } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
 import CustomCard from './customCard';
 
 
@@ -13,8 +14,61 @@ const useStyles = makeStyles(() => ({
 function ListingGrid({className, ...rest}){
     const classes = useStyles();
     return(
-        <Grid>
+        <Grid style={{margin: 15}} spacing={3}>
+        <Grid container spacing={3} display="flex" justify="center">
+            <Grid item> 
             <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+            </ Grid>
+        </Grid>
+        <Grid container spacing={3} display="flex" justify="center">
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+            </ Grid>
+        </Grid>
+        <Grid container spacing={3} display="flex" justify="center">
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+
+            </ Grid>
+            <Grid item> 
+            <CustomCard />
+            </ Grid>
+        </Grid>
+        <Grid item container display="flex" justify="center" style={{margin: 10}}> 
+        <Pagination variant="outlined" count={10} color="primary" />
+        </Grid>
         </Grid>
     )
 }
