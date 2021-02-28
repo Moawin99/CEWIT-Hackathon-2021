e281afab4558be2b187016cdcfb09391532b409f
Marcos <marlo-1990@hotmail.com>
2021-02-28 00:35:35 -0500

    adding more mock data

--- Front-End/src/mockData/data.js
+++ Front-End/src/mockData/data.js
@@ -12,6 +12,105 @@ export const sanFrancisco = [
         ]
     },
     {
+        address: "73 Hill St #A, San Francisco, CA 94110",
+        rooms: "1 bedrooms",
+        baths: "1 baths",
+        price: "3,800",
+        image: "https://photos.zillowstatic.com/fp/95eda6ed7c05035dffa60cbc8f1bea81-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "319 N Willard St, San Francisco, CA 94118",
+        rooms: "5 bedrooms",
+        baths: "1.5 baths",
+        price: "4,200",
+        image: "https://photos.zillowstatic.com/fp/f0e7d35cbbf3a688b89ea8c23db87fa4-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "2765 Mcallister St, San Francisco, CA 94118",
+        rooms: "3 bedrooms",
+        baths: "2 baths",
+        price: "3,750",
+        image: "https://photos.zillowstatic.com/fp/b2db347159510539ebe7fdbf97b385bb-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+
+    {
+        address: "619 Missouri St, San Francisco, CA 94107",
+        rooms: "3 bedrooms",
+        baths: "2 baths",
+        price: "5,400",
+        image: "https://photos.zillowstatic.com/fp/ffe653d0caaf8c4fbffc0979944ef229-cc_ft_768.jpg",
+        attractions : [
+            "Park",
+            "FB Office"
+        ]
+    },
+    {
+        address: "166 Ripley St, San Francisco, CA 94110",
+        rooms: "4 bedrooms",
+        baths: "4 baths",
+        price: "8,900",
+        image: "https://photos.zillowstatic.com/fp/5c4b327ac2eacaf1c4d50044e43fbc78-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "155 29th St, San Francisco, CA 94110",
+        rooms: "4 bedrooms",
+        baths: "4.5 baths",
+        price: "8,995",
+        image: "https://photos.zillowstatic.com/fp/7678dd6b01203a6809e78afbdc2f49fe-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "9 Ora Way, San Francisco, CA 94131",
+        rooms: "2 bedrooms",
+        baths: "3 baths",
+        price: "10,500",
+        image: "https://photos.zillowstatic.com/fp/4e4c3ef6779217d41a4bde7816736a75-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+
+    {
+        address: "2659 20th Ave, San Francisco, CA 94116",
+        rooms: "2 bedrooms",
+        baths: "1 baths",
+        price: "4,199",
+        image: "https://photos.zillowstatic.com/fp/4d9a72279697cd930668ebebbecf2b0e-cc_ft_768.jpg",
+        attractions : [
+            "Park",
+            "FB Office"
+        ]
+    },
+    {
+        address: "25 Junipero Serra Blvd, San Francisco, CA 94127",
+        rooms: "5 bedrooms",
+        baths: "3 baths",
+        price: "9,500",
+        image: "https://photos.zillowstatic.com/fp/013c61cb8c0ea306143980aab5904b53-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "347 Lisbon St, San Francisco, CA 94112",
+        rooms: "6 bedrooms",
+        baths: "6 baths",
+        price: "7,700",
+        image: "https://photos.zillowstatic.com/fp/77647ddaffc87f4941bd22986c4c6f68-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "635 Dolores St #1, San Francisco, CA 94110",
+        rooms: "2 bedrooms",
+        baths: "2 baths",
+        price: "5,995",
+        image: "https://photos.zillowstatic.com/fp/deed838b3682f9290e156b5f52316462-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+];
+
+export const paloAlto =[
+    {
         address: "2850 Middlefield Rd, Palo Alto, CA 94306",
         rooms: "2 bedrooms",
         baths: "2 baths",
@@ -33,6 +132,81 @@ export const sanFrancisco = [
         baths: "2 baths",
         price: "6,400",
         image: "https://photos.zillowstatic.com/fp/15cae66aa532398bba53b47e89d86021-cc_ft_1536.jpg",
+        attractions: []
+    },
+
+    {
+        address: "2577 Waverley St, Palo Alto, CA 94301",
+        rooms: "5 bedrooms",
+        baths: "4 baths",
+        price: "10,500",
+        image: "https://photos.zillowstatic.com/fp/17893fb9d9888fafa901fd75fbd80935-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "393 Maclane St, Palo Alto, CA 94306",
+        rooms: "3 bedrooms",
+        baths: "2.5 baths",
+        price: "7,900",
+        image: "https://photos.zillowstatic.com/fp/2bc71942f3464caa17676c6de6ed7528-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "2796 Cowper St, Palo Alto, CA 94306",
+        rooms: "3 bedrooms",
+        baths: "2 baths",
+        price: "6,700",
+        image: "https://photos.zillowstatic.com/fp/058a0611b1b7bd0076117774f4b5026f-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+
+    {
+        address: "660 Coleridge Ave, Palo Alto, CA 94301",
+        rooms: "8 bedrooms",
+        baths: "7.5 baths",
+        price: "18,900",
+        image: "https://photos.zillowstatic.com/fp/b54aec2f4c8eb337cd8745faa1f0a737-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "630 Seneca St, Palo Alto, CA 94301",
+        rooms: "4 bedrooms",
+        baths: "2 baths",
+        price: "8,295",
+        image: "https://photos.zillowstatic.com/fp/0ab5017bcbcdbe78495397bf76ac462b-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "593 Center Dr, Palo Alto, CA 94301",
+        rooms: "3 bedrooms",
+        baths: "2 baths",
+        price: "8,000",
+        image: "https://photos.zillowstatic.com/fp/e157011254ccc1aa1b38b726ba02de41-cc_ft_768.jpg",
+        attractions: []
+    },
+
+    {
+        address: "3386 Kipling St, Palo Alto, CA 94306",
+        rooms: "2 bedrooms",
+        baths: "2 baths",
+        price: "5,980",
+        image: "https://photos.zillowstatic.com/fp/e34af1f1ac33f5002112dba44372c96a-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "292 Walter Hays Dr, Palo Alto, CA 94303",
+        rooms: "4 bedrooms",
+        baths: "2.5 baths",
+        price: "7,800",
+        image: "https://photos.zillowstatic.com/fp/edc9c8ff8e87f4fb7512b879d3ee60af-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "830 Colorado Ave, Palo Alto, CA 94303",
+        rooms: "3 bedrooms",
+        baths: "2 baths",
+        price: "5,200",
+        image: "https://photos.zillowstatic.com/fp/0b1df961ea899ab460834d21a13b4188-cc_ft_768.jpg",
         attractions: []
     },
 ];
@@ -74,6 +248,83 @@ export const newYork = [
         image: "https://photos.zillowstatic.com/fp/c7b7ad0abfd21ca39155a1509b8f0183-cc_ft_768.jpg",
         attractions: []
     },
+
+    {
+        address: "17 Jerome Ave, Brooklyn, NY 11208",
+        rooms: "3 bedroom",
+        baths: "1 bath",
+        price: "2,300",
+        image: "https://photos.zillowstatic.com/fp/68c242991d857f8c62959878b79c2efc-cc_ft_768.jpg",
+        attractions : [
+            "Beach",
+            "Park"
+        ]
+    },
+    {
+        address: "6331 Pleasantview St, Flushing, NY 11379",
+        rooms: "2 bedroom",
+        baths: "1 bath",
+        price: "1,900",
+        image: "https://photos.zillowstatic.com/fp/e8707fb210573526e58cf42030be1f45-cc_ft_768.jpg",
+        attractions: [
+            "park"
+        ]
+    },
+    {
+        address: "8814 65th Dr, Flushing, NY 11374",
+        rooms: "3 bedroom",
+        baths: "1.5 bath",
+        price: "4,950",
+        image: "https://photos.zillowstatic.com/fp/d0ed4b43abd7506338f608b0b2a8b37a-cc_ft_768.jpg",
+        attractions: [
+            "park"
+        ]
+    },
+    {
+        address: "6727 52nd Rd, Flushing, NY 11378",
+        rooms: "3 bedrooms",
+        baths: "2 bath",
+        price: "2,400",
+        image: "https://photos.zillowstatic.com/fp/f51c247a891a43841d882dbe8f4692b7-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+
+    {
+        address: "123 Bay 8th St, Brooklyn, NY 11228",
+        rooms: "3 bedroom",
+        baths: "1 bath",
+        price: "2,300",
+        image: "https://photos.zillowstatic.com/fp/ad32c7d2747b485f7f7b6494a200072c-cc_ft_768.jpg",
+        attractions : [
+            "Beach",
+            "Park",
+            "FB Office"
+        ]
+    },
+    {
+        address: "1250 74th St, Brooklyn, NY 11228",
+        rooms: "3 bedroom",
+        baths: "1.5 bath",
+        price: "2,850",
+        image: "https://photos.zillowstatic.com/fp/4ea73d8b37e625433c80086ead796756-cc_ft_768.jpg",
+        attractions: []
+    },
+    {
+        address: "247 Bay 40th St, Brooklyn, NY 11214",
+        rooms: "3 bedroom",
+        baths: "1.5 bath",
+        price: "3,200",
+        image: "https://maps.googleapis.com/maps/api/streetview?location=247+Bay+40th+St%2C+Brooklyn%2C+NY+11214&size=768x576&key=AIzaSyARFMLB1na-BBWf7_R3-5YOQQaHqEJf6RQ&source=outdoor&&signature=MB8rEzO-_ONvWHcy9fISNkeVCp4=",
+        attractions: []
+    },
+    {
+        address: "38 Dare Ct, Brooklyn, NY 11229",
+        rooms: "3 bedrooms",
+        baths: "2.5 bath",
+        price: "3,000",
+        image: "https://photos.zillowstatic.com/fp/dfa3f2d5e4408a01c6d779f336e18f9e-cc_ft_768.jpg",
+        attractions: []
+    },
 ];
 
 export const seattle = [
@@ -112,5 +363,78 @@ export const seattle = [
         price: "2,100",
         image: "https://photos.zillowstatic.com/fp/79a010d21aba9dc801f36280b60c9ece-cc_ft_768.jpg",
         attractions: []
+    },
+
+    {
+        address: "700 Broadway Ave E, Seattle, WA 98102",
+        rooms: "1 bedroom",
+        baths: "1 bath",
+        price: "2,215",
+        image: "https://th.bing.com/th/id/OIP.fQmrElvF0ixV_xIjZ7FSwAHaE8?w=257&h=180&c=7&o=5&pid=1.7.jpg",
+        attractions: []
+    },
+    {
+        address: "2427 E Mcgraw St, Seattle, WA 98112",
+        rooms: "4 bedroom",
+        baths: "2 bath",
+        price: "4,500",
+        image: "https://photos.zillowstatic.com/fp/7447b4d68af068f46631cd5ea4694e0f-uncropped_scaled_within_1536_1152.webp",
+        attractions: ["park"]
+    },
+    {
+        address: "1128 Lakeview Blvd E, Seattle, WA 98102",
+        rooms: "3 bedroom",
+        baths: "2 bath",
+        price: "3,475",
+        image: "https://photos.zillowstatic.com/fp/2943b4bc08e0252066b08e9ad7223d52-cc_ft_768.jpg",
+        attractions: [
+            "lake","park"
+        ]
+    },
+    {
+        address: "2536 15th Ave S, Seattle, WA 98144",
+        rooms: "3 bedroom",
+        baths: "2 bath",
+        price: "2,995",
+        image: "https://photos.zillowstatic.com/fp/931796d5259779602add487480581a63-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "4723 35th Ave S, Seattle, WA 98118",
+        rooms: "3 bedroom",
+        baths: "-- bath",
+        price: "3,450",
+        image: "https://photos.zillowstatic.com/fp/dc92a989220ad286f6846504fb7c5c60-cc_ft_768.jpg",
+        attractions: [
+            "park"
+        ]
+    },
+    {
+        address: "1128 Lakev2408 SW Brandon St, Seattle, WA 98106",
+        rooms: "2 bedroom",
+        baths: "1 bath",
+        price: "1,950",
+        image: "https://photos.zillowstatic.com/fp/f3a879b86fbe7d50a365c29d9cb6f768-cc_ft_768.jpg",
+        attractions: [
+            "park"
+        ]
+    },
+    {
+        address: "7715 16th Ave SW, Seattle, WA 98106",
+        rooms: "5 bedroom",
+        baths: "2.5 bath",
+        price: "3,300",
+        image: "https://photos.zillowstatic.com/fp/c5995ecacf28481b5881360b054c7425-cc_ft_768.jpg",
+        attractions: ["park"]
+    },
+    {
+        address: "3069 63rd Ave SW, Seattle, WA 98116",
+        rooms: "4 bedroom",
+        baths: "3 bath",
+        price: "4,200",
+        image: "https://photos.zillowstatic.com/fp/66b9628d39afcfeb8e3911446ebf15f5-cc_ft_768.jpg",
+        attractions: [
+            "park"
+        ]
     },
 ];
